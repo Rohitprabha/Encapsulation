d2cdeca92fc570e5ab078bf9a57e7c13a3e680c5
Rohitprabha <67499119+Rohitprabha@users.noreply.github.com>
2021-05-25 19:43:23 +0530

    Add files via upload

---
+++ Access Specifiers/Protected/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Protected_Access_Specifier
+{
+    class Rectangle
+    {
+        protected double length;
+        protected double width;
+
+        double GetArea()
+        {
+            return length * width;
+        }
+        public void Display()
+        {
+            Console.WriteLine("Area: {0}", GetArea());
+        }
+    }
+    class Program : Rectangle
+    {
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.length = 4.5;
+            p.width = 3.5;
+            p.Display();
+            Console.ReadLine();
+        }
+    }
+}
