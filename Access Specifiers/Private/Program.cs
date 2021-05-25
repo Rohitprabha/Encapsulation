5f96c27f8d291e328b56f25eebc621f888e04093
Rohitprabha <67499119+Rohitprabha@users.noreply.github.com>
2021-05-25 19:41:04 +0530

    Add files via upload

---
+++ Access Specifiers/Private/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Private_Access_Specifier
+{
+    class Rectangle
+    {
+        private double length;              //access specifier defines the scope and visibility of a class member.
+        private double width;
+
+        public void details(double length, double width)
+        {
+            this.length = length;
+            this.width = width;
+        }
+        public double GetArea()
+        {
+            return length * width;
+        }
+        public void Display()
+        {
+            Console.WriteLine("Area: {0}", GetArea());
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle r = new Rectangle();
+            r.details(4.4, 3.3);
+            r.Display();
+            Console.ReadLine();
+        }
+    }
+}
