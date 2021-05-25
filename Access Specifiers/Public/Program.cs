e9aa8682f10558dfba3132b0a75e0bcdc2b3f7b4
Rohitprabha <67499119+Rohitprabha@users.noreply.github.com>
2021-05-25 19:38:44 +0530

    Add files via upload

---
+++ Access Specifiers/Public/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Public_Access_Specifier
+{
+    class Rectangle
+    {
+        public double length;
+        public double width;
+
+        public double GetArea()
+        {
+            return length * width;
+        }
+        public void Display()
+        {
+            Console.WriteLine("Area: {0}", GetArea());
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle r = new Rectangle();
+            r.length = 4.5;
+            r.width = 3.5;
+            r.Display();
+            Console.ReadLine();
+        }
+    }
+}
